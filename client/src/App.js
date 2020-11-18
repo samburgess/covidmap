adfa579552f29731867e21e08aa8fd22b149be52
samburgess <saburgess239@gmail.com>
2020-11-17 23:15:51 -0800

    client code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HeatMap from './components/HeatMap'
+
+function App() {
+  return (
+    <div className="App">
+      <HeatMap />
+    </div>
+  );
+}
+
+export default App;
